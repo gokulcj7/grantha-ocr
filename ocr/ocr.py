73be6740e7ade0db6d91b6492989215dad174cd5
gokulcj7 <61540854+gokulcj7@users.noreply.github.com>
2023-05-12 20:33:11 +0530

    Add files via upload

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,5 +1,6 @@
 import cv2
 import pytesseract
+import os
 from flask import Flask, render_template, request
 
 # Create a Flask app
@@ -15,40 +16,47 @@ def home():
 # Define a route for the OCR page
 
 
-@app.route("/ocr")
+@app.route("/ocr", methods=["GET", "POST"])
 def ocr():
-    # Get the image file from the request
-    image_file = request.files["image"]
+    if request.method == "GET":
+        return render_template("ocr.html")
+    elif request.method == "POST":
+        # Get the image file from the request
+        image_file = request.files["image"]
 
-    # Load the image
-    image = cv2.imread(image_file.filename)
+        # Save the image to disk
+        image_path = os.path.join("static", image_file.filename)
+        image_file.save(image_path)
 
-    # Convert the image to grayscale
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Load the image
+        image = cv2.imread(image_path)
 
-    # Apply thresholding to the image
-    thresholded_image = cv2.threshold(
-        grayscale_image, 127, 255, cv2.THRESH_BINARY)[1]
+        # Convert the image to grayscale
+        grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Find the contours of the image
-    contours, hierarchy = cv2.findContours(
-        thresholded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Apply thresholding to the image
+        thresholded_image = cv2.threshold(
+            grayscale_image, 127, 255, cv2.THRESH_BINARY)[1]
 
-    # Find the largest contour
-    largest_contour = max(contours, key=cv2.contourArea)
+        # Find the contours of the image
+        contours, hierarchy = cv2.findContours(
+            thresholded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Extract the bounding box of the largest contour
-    (x, y, w, h) = cv2.boundingRect(largest_contour)
+        # Find the largest contour
+        largest_contour = max(contours, key=cv2.contourArea)
 
-    # Crop the image to the bounding box
-    cropped_image = image[y:y+h, x:x+w]
+        # Extract the bounding box of the largest contour
+        (x, y, w, h) = cv2.boundingRect(largest_contour)
 
-    # Recognize the text in the image
-    text = pytesseract.image_to_string(cropped_image, lang="grantha")
+        # Crop the image to the bounding box
+        cropped_image = image[y:y+h, x:x+w]
 
-    # Return the text to the template
-    return render_template("ocr.html", text=text)
+        # Recognize the text in the image
+        text = pytesseract.image_to_string(cropped_image, lang="grantha")
+
+        # Return the text to the template
+        return render_template("ocr.html", text=text)
 
 
-if __name__ == "_main_":
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
